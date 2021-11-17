2c629adc9a601a38231b09189587ec7aa9c6f979
alebogi <no.oneplayer@gmail.com>
2021-11-17 14:06:10 +0100

    dodat button

--- background.js
+++ background.js
@@ -5,7 +5,14 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
             target: { tabId: tabId },
             files: ['content.js', 'content.css']
         });
-  
+        
+      //   if (chrome.runtime.lastError) {
+      //     var errorMsg = chrome.runtime.lastError.message
+      //     var reg = /\w*Cannot access contents of url\w*/
+      //     if (errorMsg == "Cannot access a chrome:// URL" || errorMsg.match(reg)) {
+      //         // Error handling here
+      //     }
+      //  }
     }
   })
 
--- content.js
+++ content.js
@@ -1,27 +1,44 @@
 chrome.storage.local.get(["loadNum"], function (result) {
+    console.log('loadNum currently is ' + result.loadNum);
     if (result.loadNum == undefined ) { //first time loading page, just add button
-        chrome.storage.local.set(
-            {
-                loadNum: 1
-            }
-        );
+        chrome.storage.local.set({"loadNum": 1}, function() {});
+        console.log('loadNum set 1');
         addButton();
+       
     }else{ //>1 time loading page, first remove the button, that add it again
         let newNum = result.loadNum + 1;
-        chrome.storage.local.set(
-            {
-                loadNum: newNum
-            }
-        );
+        chrome.storage.local.set({"loadNum": newNum}, function() {});
+        console.log('loadNum set ' + newNum);
         removeAll();
         addButton();
     }
 });
 
+
+
 function removeAll(){
-    document.getElementById("buttonContainer").innerHTML = "";
+    
+    let elem = document.getElementById("remindme");
+    if (elem !== undefined && elem !=null){
+        elem.remove();
+    }
+    //document.getElementById("buttonContainer").innerHTML = "";
 }
 
 function addButton(){
+    var div_elem = document.getElementsByClassName("amn")[0];
 
+    let newSpan = document.createElement("span");
+    newSpan.id = "remindme";
+    newSpan.tabIndex = "0";
+    newSpan.className = "ams remindMeClass";
+    newSpan.onclick = () => remindMeFunc();
+    newSpan.innerText = "Remind me later";
+
+    div_elem.append(newSpan);
+    
+}
+
+function remindMeFunc(){
+    console.log("REMIND ME 2")
 }
