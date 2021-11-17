ea6d6914a9b515d781d1f50e863abf1de53d705f
alebogi <no.oneplayer@gmail.com>
2021-11-17 11:52:11 +0100

    here we go again

--- content.js
+++ content.js
@@ -1,22 +1,27 @@
-// document.body.style.backgroundColor = 'orange';
-// let color = '#3aa757';
-// chrome.storage.sync.set({ color });
+chrome.storage.local.get(["loadNum"], function (result) {
+    if (result.loadNum == undefined ) { //first time loading page, just add button
+        chrome.storage.local.set(
+            {
+                loadNum: 1
+            }
+        );
+        addButton();
+    }else{ //>1 time loading page, first remove the button, that add it again
+        let newNum = result.loadNum + 1;
+        chrome.storage.local.set(
+            {
+                loadNum: newNum
+            }
+        );
+        removeAll();
+        addButton();
+    }
+});
 
-// let color = '#3aa757';
-// document.body.style.backgroundColor = color;
-// document.body.innerHTML = "hello"
+function removeAll(){
+    document.getElementById("buttonContainer").innerHTML = "";
+}
 
-var div_elem = document.getElementsByClassName("amn")[0];
-div_elem.innerHTML =  div_elem.innerHTML + '<span id="remindme"  tabindex="0" class="ams remindMeClass" > Remind me later</span>';
+function addButton(){
 
-// let newSpan = document.createElement("span");
-// newSpan.innerHTML = '<span id="remindme"  tabindex="0" class="ams remindMeClass" >Remind me later</span>';
-
-
-// newSpan.onclick = () => remindMe();
-// newSpan.className = "ams remindMeClass";
-// newButton.style.color = button.textColor;
-// newSpan.id = "remindme"
-
-
-// div_elem.appendChild(newSpan)
+}
