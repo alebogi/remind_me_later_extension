6e7d784b479648c569eb850d29092e26d35df842
alebogi <no.oneplayer@gmail.com>
2021-11-23 18:10:18 +0100

    nije resen novi red

--- options.js
+++ options.js
@@ -3,13 +3,27 @@ chrome.storage.local.get(["notifsInfo"], function (result) {
     notifsInfo = result.notifsInfo;
 
     notifsInfo.forEach((notif)=>{
-        let wrapper = document.createElement("div");
-        wrapper.className = "row";
-        wrapper.innerText = "Subject: "+ notif.mailSubject + "\n" + 
-            "From: " + notif.senderName + " (" + notif.senderEmail + ")";
+        let wrapper = document.createElement("p");
+        wrapper.setAttribute("style", "padding-left: 10px;");
+        wrapper.innerHTML = "<b>Subject:</b> "+ notif.mailSubject + "<br>" + 
+            "<b>From:</b> " + notif.senderName + " (" + notif.senderEmail + ") <br><hr><br>";
+
+        // let btn = document.createElement("button");
+        // btn.innerText = "Cancel";
+
+        // wrapper.appendChild(btn);
         
-        document.getElementById("remindersArr").appendChild(wrapper);
+        let divv = document.getElementById("remindersArr");
+        divv.appendChild(wrapper);
     
+        
     });
+
+    if(notifsInfo.length == 0){
+        let wrapper = document.createElement("span");
+        wrapper.setAttribute("style", "padding-left: 10px;");
+        wrapper.innerText = "No active reminders.";
+        document.getElementById("remindersArr").appendChild(wrapper);
+    }
 });
 
